b1ac5ef3eaee9266e9aeb22df1acd5ef3a28af21
mfis <fischermatthias1980@googlemail.com>
2015-03-22 09:47:40 +0100

    added mojo parameters

--- StaticResourceProcessingMavenPlugIn/src/main/java/mfi/staticresources/StaticResourceProcessingMavenPlugIn.java
+++ StaticResourceProcessingMavenPlugIn/src/main/java/mfi/staticresources/StaticResourceProcessingMavenPlugIn.java
@@ -1,15 +1,51 @@
 package mfi.staticresources;
 
+import java.io.File;
+
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
-@Mojo( name = "StaticResourceProcessingMavenPlugIn")
-public class StaticResourceProcessingMavenPlugIn   extends AbstractMojo  {
+@Mojo(name = "StaticResourceProcessingMavenPlugIn")
+public class StaticResourceProcessingMavenPlugIn extends AbstractMojo {
 
-    public void execute() throws MojoExecutionException
-    {
-        getLog().info( "Hello, StaticResourceProcessingMavenPlugIn." );
-    }
+	@Parameter(property = "StaticResourceProcessingMavenPlugIn.webContentSourceDir")
+	private File webContentSourceDir;
+
+	@Parameter(property = "StaticResourceProcessingMavenPlugIn.webContentDestDir")
+	private File webContentDestDir;
+
+	@Parameter(property = "StaticResourceProcessingMavenPlugIn.versionsMapFile")
+	private File versionsMapFile;
+
+	public void execute() throws MojoExecutionException {
+
+		checkParameters();
+		
+	}
+
+	private void checkParameters() {
+		
+		if (webContentSourceDir.exists()) {
+			getLog().info("webContentSourceDir = " + webContentSourceDir.getAbsolutePath());
+		} else {
+			getLog().error("webContentSourceDir = " + webContentSourceDir.getAbsolutePath() + " does not exist!");
+			throw new IllegalArgumentException("webContentSourceDir = " + webContentSourceDir.getAbsolutePath() + " does not exist!");
+		}
+
+		if (webContentDestDir.exists()) {
+			getLog().info("webContentDestDir = " + webContentDestDir.getAbsolutePath());
+		} else {
+			getLog().error("webContentDestDir = " + webContentDestDir.getAbsolutePath() + " does not exist!");
+			throw new IllegalArgumentException("webContentDestDir = " + webContentDestDir.getAbsolutePath() + " does not exist!");
+		}
+
+		if (versionsMapFile.getParentFile() != null && versionsMapFile.getParentFile().exists()) {
+			getLog().info("versionsMapFile = " + versionsMapFile.getAbsolutePath());
+		} else {
+			getLog().error("versionsMapFile parent = " + versionsMapFile.getParentFile() + " does not exist!");
+			throw new IllegalArgumentException("versionsMapFile parent = " + versionsMapFile.getParentFile() + " does not exist!");
+		}
+	}
 }
- 
